18035e8e0eb148b9611e16bcd55069f9a657904e
thisis0123 <jacknoam0@gmail.com>
2025-02-27 10:08:17 +0200

    django-authorization

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,3 +13,10 @@ def posts_list(request):
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
+
+
+
+@login_required(login_url='/users/login/')
+def post_new(request):
+    
+    return render(request,'posts/post-new.html')
